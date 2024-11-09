a38e9c9a51ef0bb543dac869a9e1a1a93569430c
cryptorichyrich <fxwisdom1@gmail.com>
2024-11-09 17:57:17 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,13 +20,15 @@ const Navbar = () => {
         <div className="text-2xl font-bold">Agustinus Biotamalo</div>
         
         {/* Hamburger button */}
-        <div className="md:hidden">
-          <button onClick={() => setIsOpen(true)} className="focus:outline-none">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-          </button>
-        </div>
+        {!isOpen && (
+          <div className="md:hidden">
+            <button onClick={() => setIsOpen(true)} className="focus:outline-none">
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            </button>
+          </div>
+        )}
 
         {/* Menu items */}
         <div className={`flex-col md:flex-row md:flex md:items-center md:space-x-6 space-y-4 md:space-y-0 ${isOpen ? 'flex' : 'hidden'} md:block`}>

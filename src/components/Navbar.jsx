3f374d7ca090602f33bbf7c7eee76c51700ae90b
cryptorichyrich <fxwisdom1@gmail.com>
2024-11-11 22:27:13 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,26 +16,28 @@ const Navbar = () => {
 
   return (
     <>
-<div id="home" style={{top:"0", width:"100%", height:"5px"}}></div>
+<div id="home" style={{ top: "0", width: "100%", height: "5px" }}></div>
 <nav className="bg-black text-white fixed w-full z-10 top-0">
-  <div className="container mx-auto flex flex-col md:flex-row justify-between items-center py-4 px-6 md:px-16 lg:px-24">
+  <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-16 lg:px-24">
     
-    {/* Brand name positioned above navigation links on mobile */}
-    <div className="text-2xl font-bold mb-4 md:mb-0 md:order-1">
-      <a href="/">Agustinus Biotamalo</a>
-    </div>
-
-    {/* Hamburger button aligned to the right on mobile */}
-    <div className="self-end md:hidden order-3">
-      <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-        </svg>
-      </button>
+    {/* Brand name and Hamburger button in the same flex container */}
+    <div className="flex items-center justify-between w-full md:w-auto">
+      <div className="text-2xl font-bold">
+        <a href="/">Agustinus Biotamalo</a>
+      </div>
+      
+      {/* Hamburger button for mobile */}
+      <div className="md:hidden">
+        <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+        </button>
+      </div>
     </div>
     
     {/* Menu items */}
-    <div className={`flex flex-col items-center md:flex-row md:items-center md:space-x-6 space-y-4 md:space-y-0 ${isOpen ? 'flex' : 'hidden'} md:block order-2`}>
+    <div className={`flex-col items-center md:flex-row md:flex md:items-center md:space-x-6 space-y-4 md:space-y-0 ${isOpen ? 'flex' : 'hidden'} md:flex`}>
       <a href="#home" className="hover:text-gray-400 text-center" onClick={() => setIsOpen(false)}>Home</a>
       <a href="#aboutMe" className="hover:text-gray-400 text-center" onClick={() => setIsOpen(false)}>About Me</a>
       <a href="#services" className="hover:text-gray-400 text-center" onClick={() => setIsOpen(false)}>Services</a>

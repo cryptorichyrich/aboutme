5400d6f6d6cd644bbd8dd3894a419b20ce9dfbb5
cryptorichyrich <fxwisdom1@gmail.com>
2024-11-11 23:10:02 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,8 +18,7 @@ const Navbar = () => {
   return (
     <><div id="home" style={{top:"0",width:"100%",height:"5px"}}></div><nav className="bg-black text-white fixed w-full z-10 top-0">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-16 lg:px-24">
-      {!isOpen && isMobile && (<div className="md:hidden text-2xl font-bold"><a href="/">Agustinus Biotamalo</a></div>)}
-      {!isMobile && (<div className="text-2xl font-bold"><a href="/">Agustinus Biotamalo</a></div>)}
+      <div className={`${isOpen && isMobile ?'md:hidden':''} text-2xl font-bold`}><a href="/">Agustinus Biotamalo</a></div>
 
         {/* Hamburger button */}
         {!isOpen && (

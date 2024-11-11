ea265c8b425db3b63f86fc65a8ec430da2f2b556
cryptorichyrich <fxwisdom1@gmail.com>
2024-11-11 22:19:06 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,15 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <><div id="home" style={{top:"0",width:"100%",height:"5px"}}></div><nav className="bg-black text-white fixed w-full z-10 top-0">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-16 lg:px-24">
-        <div className="text-2xl font-bold"><a href="/">Agustinus Biotamalo</a></div>
-
+    <><div id="home" style={{top:"0", width:"100%", height:"5px"}}></div>
+    <nav className="bg-black text-white fixed w-full z-10 top-0">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center py-4 px-6 md:px-16 lg:px-24">
+        
+        {/* Brand name */}
+        <div className="text-2xl font-bold mb-4 md:mb-0">
+          <a href="/">Agustinus Biotamalo</a>
+        </div>
+        
         {/* Hamburger button */}
         {!isOpen && (
           <div className="md:hidden">
@@ -29,9 +34,9 @@ const Navbar = () => {
             </button>
           </div>
         )}
-
+        
         {/* Menu items */}
-        <div className={`flex-col items-center md:flex-row md:flex md:items-center md:space-x-6 space-y-4 md:space-y-0 ${isOpen ? 'flex' : 'hidden'} md:block`}>
+        <div className={`flex flex-col items-center md:flex-row md:items-center md:space-x-6 space-y-4 md:space-y-0 ${isOpen ? 'flex' : 'hidden'} md:block`}>
           <a href="#home" className="hover:text-gray-400 text-center" onClick={() => setIsOpen(false)}>Home</a>
           <a href="#aboutMe" className="hover:text-gray-400 text-center" onClick={() => setIsOpen(false)}>About Me</a>
           <a href="#services" className="hover:text-gray-400 text-center" onClick={() => setIsOpen(false)}>Services</a>
@@ -42,13 +47,13 @@ const Navbar = () => {
             onClick={() => {
               handleClick('#contact');
               setIsOpen(false);
-            } }
+            }}
             className="bg-gradient-to-r from-blue-500 to-pink-500 px-4 py-2 rounded-full text-white transform transition-transform duration-300 hover:scale-105 text-center"
           >
             Whatsapp Me
           </button>
         </div>
-
+        
         {/* Close (X) button on mobile */}
         {isOpen && (
           <button
@@ -61,7 +66,8 @@ const Navbar = () => {
           </button>
         )}
       </div>
-    </nav></>
+    </nav>
+    </>
   );
 };
 

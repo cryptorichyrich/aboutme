f6607728f7608257106779898faafd667d5891f6
cryptorichyrich <fxwisdom1@gmail.com>
2024-11-11 22:48:44 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,6 +32,7 @@ const Navbar = () => {
 
         {/* Menu items */}
         <div className={`flex-col items-center md:flex-row md:flex md:items-center md:space-x-6 space-y-4 md:space-y-0 ${isOpen ? 'flex' : 'hidden'} md:block w-full`}>
+          {isOpen && (<div className="md:hidden text-2xl font-bold"><a href="/">Agustinus Biotamalo</a></div>)}
           <a href="#home" className="hover:text-gray-400 text-center w-full" onClick={() => setIsOpen(false)}>Home</a>
           <a href="#aboutMe" className="hover:text-gray-400 text-center w-full" onClick={() => setIsOpen(false)}>About Me</a>
           <a href="#services" className="hover:text-gray-400 text-center w-full" onClick={() => setIsOpen(false)}>Services</a>

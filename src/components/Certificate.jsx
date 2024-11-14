7dbe98958270028a4e0ec5a938ad80d6bb1db77b
cryptorichyrich <fxwisdom1@gmail.com>
2024-11-14 09:33:35 +0700

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -9,6 +9,12 @@ const Certificates= [
     icon: "https://media.licdn.com/dms/image/v2/D560BAQEf_NHzN2yVQg/company-logo_100_100/company-logo_100_100/0/1723593046388/udemy_logo?e=1739404800&v=beta&t=mdS7VR4FRvRmf2qWnKejxYsyCcZhyf4kgOhvj6ChDpI", // Replace with actual icon URL
   },
   {
+    name: "Create B2B Sales Cold Calling Scripts & Value Propositions",
+    issuer: "Udemy",
+    issuedDate: "Nov 2024",
+    icon: "https://media.licdn.com/dms/image/v2/D560BAQEf_NHzN2yVQg/company-logo_100_100/company-logo_100_100/0/1723593046388/udemy_logo?e=1739404800&v=beta&t=mdS7VR4FRvRmf2qWnKejxYsyCcZhyf4kgOhvj6ChDpI", // Replace with actual icon URL
+  },  
+  {
     name: "Gatsby JS v5 & Headless WordPress",
     issuer: "Udemy",
     issuedDate: "Nov 2024",
